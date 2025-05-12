e45f77f89d701198d8b23184bfb794706feffed3
notchad069 <147087780+notchad069@users.noreply.github.com>
2025-05-13 00:14:58 +0530

    Update graph_algos.py

--- graph_algos.py
+++ graph_algos.py
@@ -10,74 +10,96 @@ adj_list = {
     5: [(2, 2)]
 }
 
-def trace():
+def trace_log(action, trace_list): #this function would record the traversal 
+    print(action)
+    trace_list.append(action)
     
-    pass    
 
 #dfs
 
 def dfs(adj_list):
+    trace = [] #creatin an empty list for tracing
     source = 0 #startin from 0 which is the source node
     visited = set() #this set is needed to make sure we dont revisit the values that are already seen
-    visited.add(source)
     stack = [source] #to process erythin
+    trace_log(f"startin from node {source}", trace)
+
     while stack:
-        node = stack.pop() 
-        print(node) #processin...
-        for i,_ in adj_list.get(node,[]): 
-            if i not in visited:
-                visited.add(i)
-                stack.append(i)
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            trace_log(f"visited {node}", trace)
+            for neighbor, _ in reversed(adj_list.get(node, [])):
+                if neighbor not in visited:
+                    trace_log(f"push {neighbor} to stack", trace)
+                    stack.append(neighbor)
+
+    print("here's the trace:")
+    for step in trace:
+        print(step)
 
 #bfs
 
 from collections import deque
 
 def bfs(adj_list):
+    trace = []
     source = 0
-    visited = set()
+    visited = set([source])
     queue = deque([source])
-    visited.add(source)
+    trace_log(f"startin from {source}", trace)
+
     while queue:
         node = queue.popleft()
-        print(node)
-        for i,_ in adj_list.get(node,[]):
-            if i not in visited:
-                visited.add(i)
-                queue.append(i)
+        trace_log(f"visited {node}", trace)
+        for neighbor, _ in adj_list.get(node, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+                trace_log(f"enqueue {neighbor}", trace)
+
+    print("here's the trace:")
+    for step in trace:
+        print(step)
                 
 def dijkstra(adj_list):
     
-    dist = {node : float('inf') for node in adj_list} #initializing the dist of all nodes to infinity
-    
-    for node,edges in adj_list.items():
-        for i, _ in edges:
-            if i not in dist:
-                dist[i] = float('inf') #some of the neighbours might not have any connections but other nodes have connections to them. these are neglected in the first loop
-    source = 0
+    trace = [] 
+    dist = {} 
+    for u in adj_list: #initializing the dist of all nodes to infinity
+        dist[u] = float('inf')
+        for v, _ in adj_list[u]:
+            if v not in dist:
+                dist[v] = float('inf')
+
+    dist[0] = 0 #startin from the source node, we're gonna find the shortest dists to other nodes from the source node
     visited = set()
-    dist[source] = 0 #startin from the source node, we're gonna find the shortest dists to other nodes from the source node
-    
+    trace_log(f"startin from node 0", trace)
+
     while len(visited) < len(dist): #the loop ends when all the nodes in dist ends up in visited
-        
-        min_node = None 
+        min_node = None
         min_dist = float('inf')
-        
-        for i in dist:
-            if i not in visited and dist[i] < min_dist: #updatin those two once this satisfies
-                min_node = i
-                min_dist = dist[i]
+        for node in dist:
+            if node not in visited and dist[node] < min_dist: #updatin those two once this satisfies
+                min_node = node
+                min_dist = dist[node]
 
-        visited.add(min_node) 
-        
-        for node,weight in adj_list.get(min_node,[]):#updatin all the nodes in one go by checkin the neighbors of min_node and updatin their distances too
-            if node not in visited:
-                new_dist = min_dist + weight
-                if new_dist < dist[node]:
-                    dist[node] = new_dist
-    for node in dist:
-        print(f"{node} : {dist[node]}")
+        if min_node is None:
+            break
 
+        visited.add(min_node)
+        trace_log(f"visitin node {min_node} with current distance of {min_dist}", trace)
+
+        for neighbor, weight in adj_list.get(min_node, []):#updatin all the nodes in one go by checkin the neighbors of min_node and updatin their distances too
+            if neighbor not in visited:
+                new_dist = dist[min_node] + weight
+                if new_dist < dist[neighbor]:
+                    dist[neighbor] = new_dist
+                    trace_log(f"updatin distance for node {neighbor} to {new_dist}", trace)
+
+    print("here's the trace:")
+    for step in trace:
+        print(step)
 #mst
 
 class DSU:
@@ -98,87 +120,99 @@ class DSU:
 
 
 def kruskal_mst(adj_list):
+    trace = []
     edges = [] #we're bouta create a tuple that contains the weight of the edge and the two nodes that the edge connects
-    
     for u in adj_list:
-        for v,w in adj_list[u]:
-            edges.append([w,u,v])
-            
+        for v, w in adj_list[u]:
+            edges.append((w, u, v))
+
     edges.sort() #we're sorting the edge list here
-    
     nodes = set() #this set will hold all the nodes
-    
-    for _,u,v in edges:
-        nodes.update([u,v])
-    
-    dsu = DSU(max(nodes) + 1 )
+    for _, u, v in edges:
+        nodes.update([u, v])
+
+    dsu = DSU(max(nodes) + 1)
     mst = [] #we'll enter the edges that would be used to take that path 
     total_weight = 0 #initiatin the total weight to zero so that we could get the total weight at the end of the function
-    
-    for w,u,v in edges:
-        if dsu.union(u,v):
-            mst.append((u,v,w))
-            total_weight+=1
-    return mst,total_weight
+ 
+    for w, u, v in edges:
+        if dsu.union(u, v):
+            mst.append((u, v, w))
+            total_weight += w
+            trace_log(f"add edge {u} -> {v} (weight {w})", trace)
+        else:
+            trace_log(f"skip edge {u} -> {v} (would form a cycle)", trace)
+
+    print("here's the trace:")
+    for step in trace:
+        print(step)
+    print(f"total weight: {total_weight}")
+    return mst, total_weight 
+
 
 #prim's mst
 
 def prim_mst(adj_list):
-     
+    trace = []
     all_nodes = set(adj_list.keys()) #adding all the nodes to a set
-    for i in adj_list.values():
-        for u,w in i:
-            all_nodes.add(u)
-    
+    for neighbors in adj_list.values():
+        for node, _ in neighbors:
+            all_nodes.add(node)
+
     visited = set()
     source = 0
     visited.add(source)
-    
     mst = []
     total_weight = 0
-    
-    edges = []
-    for u in adj_list:
-        for v,w in adj_list[u]:
-            edges.append([w,u,v])
+
+    trace_log(f"startin from node {source}", trace)
+
     while len(visited) < len(all_nodes):
         min_edge = None
         min_weight = float('inf')
-        for u in visited:
+
+        for u in visited:  # findin the minimum weight edge from visited to unvisited
             for v, w in adj_list.get(u, []):
                 if v not in visited and w < min_weight:
                     min_edge = (u, v, w)
                     min_weight = w
-        
+
         if min_edge is None:
             break
+
+        u, v, w = min_edge
         visited.add(v)
-        mst.append((u,v,w))
+        mst.append((u, v, w))
         total_weight += w
-        
-        for to, weight in adj_list.get(v, []):
-            if to not in visited:
-                edges.append((v, to, weight))
-        
-        for u,v,w in mst:
-            print(f"{u} -> {v} (weight {w})")
-        
-        print("total weight : ",total_weight)
-    
+        trace_log(f"add edge {u} -> {v} (weight {w})", trace)
+
+    print("here's the trace")
+    for step in trace:
+        print(step)
+    print(f"total weight: {total_weight}")
+
         
     
 
 if __name__ == "__main__":
     print("dfs")
     dfs(adj_list)
+    print('\n')
+    
     print("bfs")
     bfs(adj_list)
+    print('\n')
+    
     print("dijakstra")
     dijkstra(adj_list)
+    print('\n')
+
     print("kruskal's mst")
     mst,weight = kruskal_mst(adj_list)
     for u,v,w in mst:
         print(f"{u} -> {v} (weight of {w})")
     print(f"total weight is : ",weight)
-    print("mst")
+    print('\n')
+
+    print("prim's mst")
     prim_mst(adj_list)
